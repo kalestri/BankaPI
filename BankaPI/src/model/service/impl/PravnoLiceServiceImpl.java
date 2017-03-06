9738dfe315581c92a96b9c5bd25c595433a146eb
Pivin <aleksandarpivin@gmail.com>
2017-03-06 21:54:37 +0100

    aaa

--- BankaPI/src/model/service/impl/PravnoLiceServiceImpl.java
+++ BankaPI/src/model/service/impl/PravnoLiceServiceImpl.java
@@ -2,7 +2,7 @@ package model.service.impl;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
+
 
 import model.PravnoLice;
 import model.dao.PravnoLiceDao;
@@ -12,31 +12,27 @@ public class PravnoLiceServiceImpl implements PravnoLiceService {
 //@Autowired
 	private PravnoLiceDao pravnoLiceDao;
 	
-	
-	@Transactional
+
 	public void add(PravnoLice pravnoLice) {
 		pravnoLiceDao.add(pravnoLice);
 
 	}
 
-	@Transactional
 	public void edit(PravnoLice pravnoLice) {
 		pravnoLiceDao.edit(pravnoLice);
 
 	}
 
-	@Transactional
+
 	public void delete(int pib) {
 		pravnoLiceDao.delete(pib);
 
 	}
 
-	@Transactional
 	public PravnoLice getPravnoLice(int pib) {
 		return pravnoLiceDao.getPravnoLice(pib);
 	}
 
-	@Transactional
 	public List getPravnaLica() {
 		return pravnoLiceDao.getPravnaLica();
 	}
